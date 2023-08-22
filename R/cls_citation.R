d1dce2e073b7e9447e4121cab266a86be32fa117
Jamie Lendrum <jalendrum@gmail.com>
2023-08-22 19:13:17 +0100

    Finish citation class

--- R/cls_citation.R
+++ R/cls_citation.R
@@ -9,6 +9,36 @@ NULL
 #' @return An S7 object representing a GEDCOM SOURCE_CITATION.
 #' @export
 #' @include cls_date.R cls_translation.R cls_media_link.R cls_note.R
+#' @tests
+#' expect_error(class_citation(), regexp = "@sour_uid has too few elements")
+#' expect_error(class_citation("94a8ddf6-410d-11ee-8593-185680d6e741",
+#'                             event_phrase = "phrase"),
+#'              regexp = "@event_phrase requires a @event_types")
+#' expect_error(class_citation("94a8ddf6-410d-11ee-8593-185680d6e741",
+#'                             role = "HUSB"),
+#'              regexp = "@role requires a @event_types")
+#' expect_error(class_citation("94a8ddf6-410d-11ee-8593-185680d6e741",
+#'                             event_types = "BIRT", role_phrase = "phrase"),
+#'              regexp = "@role_phrase requires a @role")
+#' expect_error(class_citation("94a8ddf6-410d-11ee-8593-185680d6e741",
+#'                             certainty = "4"),
+#'              regexp = "@certainty has an invalid value")
+#' expect_error(class_citation("94a8ddf6-410d-11ee-8593-185680d6e741",
+#'                             notes = ""),
+#'              regexp = "@notes is in an invalid format")
+#' expect_snapshot_value(class_citation("94a8ddf6-410d-11ee-8593-185680d6e741",
+#'                                      where = "page 2",
+#'                                      date = "2 JUN 2006",
+#'                                      source_text = c("verbatim","text"),
+#'                                      event_types = "BIRT, MARR, DEAT",
+#'                                      event_phrase = "and others",
+#'                                      role = "HUSB",
+#'                                      role_phrase = "phrase",
+#'                                      certainty = "3",
+#'                                      media_links = class_media_link("9ec2befb-250f-48c6-b5ce-ae342c3775ad"),
+#'                                      note_uids = c("ff76d1d9-46ed-4bae-a900-39728b4f59bd",
+#'                                                    "b5093460-f77a-4b64-b69d-ede4d2a07f7e"),
+#'                                      notes = c("these are","some notes"))@as_ged, "json2")
 class_citation <- S7::new_class(
   "class_citation",
   package = "gedcomS7",
@@ -17,7 +47,7 @@ class_citation <- S7::new_class(
     where = S7::class_character,
     date = S7::class_character | class_date_value,
     source_text = S7::class_list | class_translation_txt | S7::class_character,
-    event_type = S7::class_character,
+    event_types = S7::class_character,
     event_phrase = S7::class_character,
     role = S7::class_character,
     role_phrase = S7::class_character,
@@ -36,7 +66,7 @@ class_citation <- S7::new_class(
                 length(self@source_text) > 0),
           obj_to_ged(self@date, "DATE") |> increase_level(by = 2),
           obj_to_ged(self@source_text, "TEXT") |> increase_level(by = 2),
-          sprintf("1 EVEN %s", self@event_type),
+          sprintf("1 EVEN %s", self@event_types),
           sprintf("2 PHRASE %s", self@event_phrase),
           sprintf("2 ROLE %s", self@role),
           sprintf("3 PHRASE %s", self@role_phrase),
@@ -53,23 +83,23 @@ class_citation <- S7::new_class(
       chk_input_size(self@sour_uid, "@sour_uid", 1, 1),
       chk_input_size(self@where, "@where", 0, 1, 1),
       chk_input_size(self@date, "@date", 0, 1),
-      chk_input_size(self@event_type, "@event_type", 0, 1),
-      chk_input_size(self@date_phrase, "@event_phrase", 0, 1, 1),
+      chk_input_size(self@event_types, "@event_types", 0, 1),
+      chk_input_size(self@event_phrase, "@event_phrase", 0, 1, 1),
       chk_input_size(self@role, "@role", 0, 1),
       chk_input_size(self@role_phrase, "@role_phrase", 0, 1, 1),
       chk_input_size(self@certainty, "@certainty", 0, 1),
       chk_input_parents(self@event_phrase, "@event_phrase", 
-                        self@event_type, "@event_type"),
+                        self@event_types, "@event_types"),
       chk_input_parents(self@role, "@role", 
-                        self@event_type, "@event_type"),
+                        self@event_types, "@event_types"),
       chk_input_parents(self@role_phrase, "@role_phrase", 
                         self@role, "@role"),
       chk_input_pattern(self@sour_uid, "@sour_uid", reg_uuid(TRUE)),
       chk_input_pattern(self@note_uids, "@note_uids", reg_uuid(TRUE)),
-      chk_input_choice(self@event_type, "@event_type", val_fact_types()),
+    #  chk_input_choice(self@event_types, "@event_types", val_fact_types()), it's a comma separated value
       chk_input_choice(self@role, "@role", val_roles()),
       chk_input_choice(self@certainty, "@certainty", val_certainty()),
-      chk_input_S7classes(self@source_text, "@source_text", class_translation_txt),
+      chk_input_S7classes(self@source_text, "@source_text", class_translation_txt, ".+"),
       chk_input_S7classes(self@media_links, "@media_links", class_media_link, reg_uuid(TRUE)),
       chk_input_S7classes(self@notes, "@notes", class_note, ".+")
     )
---
+++ tests/testthat/_snaps/roxytest-tests-cls_citation.md
@@ -0,0 +1,8 @@
+# Function class_citation() @ L43
+
+    {
+      "type": "character",
+      "attributes": {},
+      "value": ["0 SOUR 94a8ddf6-410d-11ee-8593-185680d6e741", "1 PAGE page 2", "1 DATA", "2 DATE 2 JUN 2006", "2 TEXT verbatim", "2 TEXT text", "1 EVEN BIRT, MARR, DEAT", "2 PHRASE and others", "2 ROLE HUSB", "3 PHRASE phrase", "1 QUAY 3", "1 OBJE 9ec2befb-250f-48c6-b5ce-ae342c3775ad", "1 SNOTE ff76d1d9-46ed-4bae-a900-39728b4f59bd", "1 SNOTE b5093460-f77a-4b64-b69d-ede4d2a07f7e", "1 NOTE these are", "1 NOTE some notes"]
+    }
+
