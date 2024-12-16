1b8d6ca08e754eeb6d944a7b29bc819c55f1ca91
Jamie Lendrum <jalendrum@gmail.com>
2024-12-16 17:36:39 +0000

    Add comments to methods register

--- R/zzz.R
+++ R/zzz.R
@@ -10,6 +10,12 @@
     
 }
 
+# This is S7’s way of registering methods, rather than using export directives in 
+# your NAMESPACE like S3 and S4 do. This is only strictly necessary if registering 
+# methods for generics in other packages, but there’s no harm in adding it and it 
+# ensures that you won’t forget later. (And if you’re not importing S7 into your 
+# namespace it will quiet an R CMD check NOTE.)
+# https://cran.r-project.org/web/packages/S7/vignettes/packages.html
 .onLoad <- function(...) {
   S7::methods_register()
 }
