3a2b52c2bb14654224147d7b683a394ad46cd08a
Jamie Lendrum <jalendrum@gmail.com>
2024-12-19 00:18:51 +0000

    Remove startup message - it's in the README

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,4 @@
 
-.onAttach <- function(libname, pkgname) {
-  
-    packageStartupMessage(
-      "When importing existing GEDCOM files, you should ensure that they ",
-      "are error free.\nThis package assumes imported GEDCOM files are valid and ",
-      "very few validation checks are carried out.\nSeveral GEDCOM validators are available, ",
-      "including an online validator at https://ged-inline.org/"
-    )
-    
-}
-
 # This is S7’s way of registering methods, rather than using export directives in 
 # your NAMESPACE like S3 and S4 do. This is only strictly necessary if registering 
 # methods for generics in other packages, but there’s no harm in adding it and it 
