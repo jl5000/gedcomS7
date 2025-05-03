25bf78ffc711ebc2d40aeaf0fb5ec61054d815c9
Jamie Lendrum <jalendrum@gmail.com>
2025-05-03 23:39:36 +0100

    Add tests for get_record_type()

--- R/fn_is.R
+++ R/fn_is.R
@@ -7,30 +7,38 @@ is_media_rec <- function(record) S7::S7_inherits(record, MediaRecord)
 is_note_rec <- function(record) S7::S7_inherits(record, NoteRecord)
 is_subm_rec <- function(record) S7::S7_inherits(record, SubmitterRecord)
 
-is_indi_xref <- function(x, xref) xref %in% x@records@XREFS[["INDI"]]
-is_fam_xref <- function(x, xref) xref %in% x@records@XREFS[["FAM"]]
-is_sour_xref <- function(x, xref) xref %in% x@records@XREFS[["SOUR"]]
-is_repo_xref <- function(x, xref) xref %in% x@records@XREFS[["REPO"]]
-is_media_xref <- function(x, xref) xref %in% x@records@XREFS[["OBJE"]]
-is_note_xref <- function(x, xref) xref %in% x@records@XREFS[["SNOTE"]]
-is_subm_xref <- function(x, xref) xref %in% x@records@XREFS[["SUBM"]]
+is_indi_xref <- function(x, xref) xref %in% x@records@XREFS[[val_record_types()[["Individual"]]]]
+is_fam_xref <- function(x, xref) xref %in% x@records@XREFS[[val_record_types()[["Family"]]]]
+is_sour_xref <- function(x, xref) xref %in% x@records@XREFS[[val_record_types()[["Source"]]]]
+is_repo_xref <- function(x, xref) xref %in% x@records@XREFS[[val_record_types()[["Repository"]]]]
+is_media_xref <- function(x, xref) xref %in% x@records@XREFS[[val_record_types()[["Multimedia"]]]]
+is_note_xref <- function(x, xref) xref %in% x@records@XREFS[[val_record_types()[["Note"]]]]
+is_subm_xref <- function(x, xref) xref %in% x@records@XREFS[[val_record_types()[["Submitter"]]]]
 
+#' @tests
+#' expect_equal(get_record_type(IndividualRecord()), "INDI")
+#' expect_equal(get_record_type(FamilyRecord()), "FAM")
+#' expect_equal(get_record_type(SourceRecord()), "SOUR")
+#' expect_equal(get_record_type(RepositoryRecord(repo_name = "Lib")), "REPO")
+#' expect_equal(get_record_type(SubmitterRecord(subm_name = "subm")), "SUBM")
+#' expect_equal(get_record_type(NoteRecord(text = "text")), "SNOTE")
+#' expect_equal(get_record_type(MediaRecord(files = MediaFile("file.ext", media_type = "type"))), "OBJE")
 get_record_type <- function(record){
   
-  if(S7::S7_inherits(record, IndividualRecord)){
-    "INDI"
-  } else if(S7::S7_inherits(record, FamilyRecord)){
-    "FAM"
-  } else if(S7::S7_inherits(record, SourceRecord)){
-    "SOUR"
-  } else if(S7::S7_inherits(record, RepositoryRecord)){
-    "REPO"
-  } else if(S7::S7_inherits(record, MediaRecord)){
-    "OBJE"
-  } else if(S7::S7_inherits(record, NoteRecord)){
-    "SNOTE"
-  } else if(S7::S7_inherits(record, SubmitterRecord)){
-    "SUBM"
+  if(is_indi_rec(record)){
+    val_record_types()[["Individual"]]
+  } else if(is_fam_rec(record)){
+    val_record_types()[["Family"]]
+  } else if(is_sour_rec(record)){
+    val_record_types()[["Source"]]
+  } else if(is_repo_rec(record)){
+    val_record_types()[["Repository"]]
+  } else if(is_media_rec(record)){
+    val_record_types()[["Multimedia"]]
+  } else if(is_note_rec(record)){
+    val_record_types()[["Note"]]
+  } else if(is_subm_rec(record)){
+    val_record_types()[["Submitter"]]
   } else {
     stop("Unrecognised record")
   }
